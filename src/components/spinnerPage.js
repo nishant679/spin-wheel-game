528214a5ff0aaf008b49cdc380955933b87aea6b
Nishant <nishant@eldmandate.co>
2022-12-05 03:06:50 +0530

    added spin page

--- src/components/spinnerPage.js
+++ src/components/spinnerPage.js
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "../scss/spinnerPage.scss";
 import spinWheel from "../assets/spinner.png";
+import clap from "../assets/cheer.mp3";
+import { useHistory } from "react-router-dom";
+import Confetti from "react-confetti";
+const audio1 = new Audio(clap);
 
 const SpinnerPage = () => {
   const [spin, setSpin] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const { width, height } = [300, 200];
+  const history = useHistory();
 
   const rotateWheel = () => {
     setSpin(true);
     setTimeout(() => {
+      setShowConfetti(true);
+      audio1.play();
+    }, 2000);
+    setTimeout(() => {
       setSpin(false);
     }, 4000);
+    setTimeout(() => {
+      setShowConfetti(false);
+      audio1.pause();
+      history.push("/");
+    }, 8000);
   };
   return (
     <div className="spinner-main">
+      {showConfetti && <Confetti width={width} height={height} run={true} />}
       <div className="wheel-container">
         <img
           src={spinWheel}
